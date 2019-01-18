1cc269c9b2e6aa3699606ef0bdcb7100d6875669
yesan0327 <353575688@qq.com>
2019-01-18 22:19:50 +0800

    2019.1.18 upload 部分

--- ManagementWebsite/urls.py
+++ ManagementWebsite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from upload import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('upload/', include('upload.urls')),
     path('admin/', admin.site.urls),
+
 ]
