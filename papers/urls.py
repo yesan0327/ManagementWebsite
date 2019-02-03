59eb14f1960ce49be82cd9ff95454e82d33b207a
yesan0327 <353575688@qq.com>
2019-02-03 16:42:27 +0800

    添加一些文章分类button

--- papers/urls.py
+++ papers/urls.py
@@ -3,5 +3,10 @@ from papers import views
 
 urlpatterns = [
     path('API/', views.index, name='index'),
+    path('inputcode/', views.inputcode, name='inputcode'),
+    path('outputcode/', views.outputcode, name='outputcode'),
+    path('function/', views.function, name='function'),
+    path('2013/', views.time, name='time'),
+    path('ASE/', views.ase, name='ase'),
     path('all/', views.all, name='all'),
 ]
