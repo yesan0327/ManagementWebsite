8670219794cca0d307e36941e8318b26a13516f2
yesan0327 <353575688@qq.com>
2019-03-18 11:31:54 +0800

    上传功能

--- ManagementWebsite/views.py
+++ ManagementWebsite/views.py
@@ -9,8 +9,9 @@ from django.db.models import Q
 
 
 def home(request):
+
     paper0 = Paper.objects.filter(conference__contains='ASE').filter(time=2013).count()
     paper1 = Paper.objects.filter(conference__contains='ASE').filter(time=2014).count()
     paper2 = Paper.objects.filter(conference__contains='ASE').filter(time=2011).count()
     paper3 = Paper.objects.filter(conference__contains='ASE').filter(time=2007).count()
-    return render(request, 'home.html', {'paper0': paper0, 'paper1': paper1, 'paper2': paper2, 'paper3': paper3})
+    return render(request, 'home.html', {'paper0': paper0})
--- upload/urls.py
+++ upload/urls.py
@@ -3,4 +3,5 @@ from upload import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('uploadFile/', views.upload_file, name='upload_file'),
 ]
